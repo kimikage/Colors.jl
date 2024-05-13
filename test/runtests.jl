61bcf8a78f2a369ee336da156fe5cfaac588581d
kimikage <kimikage.ceo@gmail.com>
2024-05-13 17:41:32 +0900

    [DoNotMerge] test downstream packages

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,40 @@
-using Colors, Test
+# using Colors, Test
+using Pkg
+
+Pkg.activate("colors", shared = true)
+Pkg.develop(PackageSpec(path = joinpath(@__DIR__, "..")))
+
+try
+    Pkg.add("ColorVectorSpace")
+    Pkg.test("ColorVectorSpace")
+catch
+end
+
+try
+    Pkg.add("ImageCore")
+    Pkg.test("ImageCore")
+catch
+end
+
+try
+    Pkg.add("Images")
+    Pkg.test("Images")
+catch
+end
+
+try
+    Pkg.add("Cairo")
+    Pkg.test("Cairo")
+catch
+end
+
+try
+    Pkg.add("Makie")
+    Pkg.test("Makie")
+catch
+end
+
+#=
 @test isempty(detect_ambiguities(Colors, Base, Core))
 
 include("algorithms.jl")
@@ -10,3 +46,5 @@ include("din99.jl")
 include("display.jl")
 include("parse.jl")
 include("utilities.jl")
+
+=#
